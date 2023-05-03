f45fe5b976bafa03502bf88c32c74d017a3c1aed
qjz66 <2199921383@qq.com>
2023-05-03 23:29:10 +0800

    P1013 done

--- P1013.cpp
+++ P1013.cpp
@@ -2,35 +2,59 @@
 #include "string"
 using namespace std;
 
+int a[200], b[200];//a b 分别存储翻转和未翻转数字
+string s;//s存字符串
+
+void trans(int l);//将字符串转化为对应进制整数存入a
+void turn(int l);//将a翻转存入b
+int add(int n, int l);//高精度加
+bool judge(int l);//判断是否为回文
+
 int main(){
-    int n, m, flag;
-    cin >> n >> m;//n为指定的进制,m为初始数字；
-    for(int i = 1;i <= 30;i++){//至多循环30次
-        int l, c, d;//存字符串长度，方便得到反串
-        string a, b, e;//a存m转化为的字符串,b存他的相反串
-        flag = 1;
-        a = to_string(m);
-        b = to_string(m);
-        l = int(a.length());
-        for(int j = 0; j < l; j++){//得到反串
-            b[j] = a[l - j - 1];
-        }
-        c = stoi(a, 0, n);
-        d = stoi(b, 0, n);
-        m = c + d;//得到新的m
-        e = to_string(m);
-        l = e.length();
-        for(int m = 0;m < l / 2;m++){
-            if(e[m] != e[l - m - 1]){//若不为回文，将flag置0并退出循环
-                flag = 0;
-                break;
-            }
-        }
-        if(flag){
-            cout << "STEP=" << i;
+    int n, l, ans;//n为进制数，l为字符串长度，ans为步骤数
+    cin >> n >> s;
+    l = s.length();
+    trans(l);//将字符串转化为数字只做一次，后续的判断，相加等都在a中进行，省去互相转换的步骤
+    for(ans = 1;ans <= 30;ans++){
+        turn(l);
+        l = add(n, l);//得到新数字的长度
+        if(judge(l)){//若为回文串则输出ans
+            cout << "STEP=" << ans;
             return 0;
         }
     }
-    cout << "Impossible!";
+    cout << "Impossible!";//从循环中出来只可能是ans > 30
     return  0;
+}
+
+void trans(int l){
+    for(int i = 0;i < l;i++){
+        if(s[l - i - 1] >= '0' && s[l - i - 1] <= '9')//若为0-9则 - ‘0’ 将其转化为数字
+            a[i] = s[l - i - 1] - '0';
+        else//为A-F则- 'A' + 10转化为数字
+            a[i] = s[l - i - 1] - 'A' + 10;
+    }
+}
+
+void turn(int l){
+    for(int i = 0;i < l;i++){
+        b[i] = a[l - i - 1];
+    }
+}
+
+int add(int n, int l){
+    for(int i = 0;i < l;i++){
+        a[i + 1] += (a[i] + b[i]) / n;//先算a[i + 1]以免a[i]被改变
+        a[i] = (a[i] + b[i]) % n;//mod n
+    }
+    while(a[l] != 0) l++;//更新l
+    return l;
+}
+
+bool judge(int l){
+    for(int i = 0;i < l / 2;i++){
+        if(a[i] != a[l - i - 1])//只要有一个不相等返回false
+            return false;
+    }
+    return true;
 }
